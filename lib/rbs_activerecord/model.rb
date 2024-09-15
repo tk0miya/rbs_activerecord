179cc3a204b2fd455792e6b8c8b32e75d40d1c78
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-09-15 17:52:55 +0900

    Generator::Attributes now supports attributes via ActiveRecord::Attributes

--- lib/rbs_activerecord/model.rb
+++ lib/rbs_activerecord/model.rb
@@ -9,9 +9,10 @@ module RbsActiverecord
     attr_reader :klass #: singleton(ActiveRecord::Base)
 
     # @rbs!
+    #   def attribute_types: () -> Hash[String, untyped]
     #   def columns: () -> Array[untyped]
     #   def reflect_on_all_associations: (Symbol) -> Array[untyped]
-    def_delegators :klass, :columns, :reflect_on_all_associations
+    def_delegators :klass, :attribute_types, :columns, :reflect_on_all_associations
 
     # @rbs klass: singleton(ActiveRecord::Base)
     def initialize(klass) #: void
