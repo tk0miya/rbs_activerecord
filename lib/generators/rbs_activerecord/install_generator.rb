bbb37c160928de0f2e9bb8a9c8e62d597f2f069b
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-11-09 13:29:33 +0900

    Implement RbsActiverecord::InstallGenerator

---
+++ lib/generators/rbs_activerecord/install_generator.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "rails"
+
+module RbsActiverecord
+  class InstallGenerator < Rails::Generators::Base
+    def create_raketask
+      create_file "lib/tasks/rbs_activerecord.rake", <<~RUBY
+        # frozen_string_literal: true
+
+        begin
+          require "rbs_activerecord/rake_task"
+
+          RbsActiverecord::RakeTask.new
+        rescue LoadError
+          # failed to load rbs_activerecord. Skip to load rbs_activerecord tasks.
+        end
+      RUBY
+    end
+  end
+end
